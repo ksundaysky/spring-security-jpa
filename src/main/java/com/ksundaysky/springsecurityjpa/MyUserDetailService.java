55894eb6cb933197d18fb1d558cbe81535a16ef8
Krzysztof Niedzielski <krzysztof.niedzielski96@gmail.com>
2020-06-24 21:46:11 +0200

    add User details service

---
+++ src/main/java/com/ksundaysky/springsecurityjpa/MyUserDetailService.java
@@ -0,0 +1,12 @@
+package com.ksundaysky.springsecurityjpa;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return new MyUserDetails(s);
+    }
+}
--- src/main/java/com/ksundaysky/springsecurityjpa/SecurityConfiguration.java
+++ src/main/java/com/ksundaysky/springsecurityjpa/SecurityConfiguration.java
@@ -1,14 +1,19 @@
 package com.ksundaysky.springsecurityjpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.userDetailsService(userDetailsService)
     }
 
     @Override
